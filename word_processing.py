ac7424aac44c8a40c911143755f8b758839da5a7
EsdeathEsdeath <alcatraz.au@gmail.com>
2020-02-04 19:16:39 +0300

    Update word_processing.py

---
+++ word_processing.py
@@ -0,0 +1,14 @@
+# Import Library of Gaussian Naive Bayes model
+from sklearn.naive_bayes import GaussianNB
+import numpy as np
+
+x = np.array()
+Y = np.array()
+model = GaussianNB()
+
+# Тренировка модели
+model.fit(x, y)
+
+# Predict Output
+predicted = model.predict()
+print(predicted)
